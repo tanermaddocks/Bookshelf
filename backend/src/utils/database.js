e5b7ec642b155ff48494ad19e66e06324e5548d3
tanermaddocks <maddockstaner@gmail.com>
2025-06-04 20:13:11 +1000

    test: Added testing for health route

--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -1,13 +1,21 @@
 const mongoose = require("mongoose");
 
+// Define database URL
+const dbName = process.env.npm_package_name;
+const dbURL = process.env.DB_URL || "mongodb://localhost:27017/" + dbName;
+
 // Function to connect to database
-async function connectDB (dbURL) {
+async function connectDB () {
   try {
+
     await mongoose.connect(dbURL);
     console.log("Connected to database: " + dbURL);
+
   } catch (error) {
+
     console.error("Database connection error: " + error.message);
     process.exit(1);
+    
   }
 };
 
