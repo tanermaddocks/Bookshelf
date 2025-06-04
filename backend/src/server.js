0f3ffa754c698957021ab07956d924b1c2040716
tanermaddocks <maddockstaner@gmail.com>
2025-06-04 16:54:58 +1000

    feat: Made a working connection to a local database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,15 @@
 const app = require("./index.js");
 const connectDB = require("./utils/database.js");
 
+// Define the port
 const PORT = process.env.PORT || 3000;
 
+// Define database URL
+const dbName = process.env.npm_package_name;
+const dbURL = process.env.DB_URL || "mongodb://localhost:27017/" + dbName;
+
+// Run the server 
 app.listen(PORT, async () => {
-  // await connectDB();
+  await connectDB(dbURL);
   console.log("Bookshelf server is running on port:" + PORT);
 });
